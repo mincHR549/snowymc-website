19e3dd35add741b0160d682eeee4dc9d37b74392
mincHR549 <152802566+mincHR549@users.noreply.github.com>
2025-10-19 12:41:27 +0800

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.snowymc.top', // 域名
+  generateRobotsTxt: true,            // 自动生成 robots.txt
+  sitemapSize: 7000,                  // 每个 sitemap 最大 URL 数
+}
